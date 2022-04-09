0fc949ea379e4a2584fdf0c60a400961fa61256b
tayeful912 <kazitayefulislam12@gmail.com>
2022-04-09 23:45:57 +0600

    useFirebase hooks added

--- src/conponents/Header/Header.js
+++ src/conponents/Header/Header.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import useFirebase from '../../hooks/useFirebase';
 import './Header.css';
 
 const Header = () => {
+
+    const { user } = useFirebase()
     return (
         <div className='header'>
 
@@ -10,8 +13,17 @@ const Header = () => {
                 <Link to='/'>Home</Link>
                 <Link to='/products'>Products</Link>
                 <Link to='/orders'>Orders</Link>
-                <Link to='/login'>Login</Link>
+
                 <Link to='/register'>Register</Link>
+
+                {
+                    user.uid ? <button>Log Out</button> : <Link to='/login'>Login</Link>
+
+                }
+
+
+
+
             </nav>
 
 
--- src/conponents/Login/Login.js
+++ src/conponents/Login/Login.js
@@ -1,24 +1,26 @@
 import React from 'react';
+import useFirebase from '../../hooks/useFirebase';
 
 const Login = () => {
+
+    const { signInWithGoogle, user } = useFirebase()
     return (
         <div>
 
-            <h1>This Is Login</h1>
+            <h1>This Is Login {user.displayName}</h1>
+            <div style={{ margin: '20px' }}>
+                <button onClick={signInWithGoogle}>Sign In With Google</button>
+            </div>
+
 
             <form >
-
-                <div style={{ margin: '20px' }}>
-                    <input type="button" value="Google sign in" />
-                </div>
-
 
                 <input type="email" placeholder='Your Email' />
                 <br />
                 <input type="password" placeholder='your Password' />
                 <br />
 
-                <input type="button" value="Login" />
+                <input onClick={signInWithGoogle} type="button" value="Login" />
 
 
             </form>
